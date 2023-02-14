dc7dba765d2393634a7ec64b622cc9823f25c29d
Caen De Silva <caen@desilva.se>
2023-02-14 22:36:57 +0100

    Set min height to screen and push footer to bottom

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -6,10 +6,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>{{ $title ?? config('app.name') }}</title>
 </head>
-<body class="d-flex flex-column">
+<body class="d-flex flex-column vh-100">
     <x-nav />
 
-    <main class="container py-4">
+    <main class="container py-4 mb-auto">
         {{ $slot }}
     </main>
 
